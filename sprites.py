da6dcaf911389cb44ab8adb96c5aa85d248cd7c5
Raymond <jzhu000@citymail.cuny.edu>
2015-05-08 13:48:42 -0400

    Made Platform, Cloud, Brick classes. Also made a simple tile map.

---
+++ sprites.py
@@ -0,0 +1,31 @@
+from pygame.locals import *
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        self.image = pygame.image.load("data/brick1.png").convert()
+        self.x = x
+        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.x,self.y]
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        self.image = pygame.image.load("data/platform-top.png").convert()
+        self.x = x
+        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.x,self.y]
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        self.image = pygame.image.load("data/cloud.png").convert()
+        self.x = x
+        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.x,self.y]
+
+
+
