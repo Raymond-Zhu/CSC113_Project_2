ae54bbc03f7ad6639908f8ec68aebf8bd54b0fe1
Raymond <jzhu000@citymail.cuny.edu>
2015-05-08 14:25:56 -0400

    Some basic main stuff

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 
+pygame.init()
+SCREEN_SIZE(640,480)
+screen = pygame.display.set_mode(SCREEN_SIZE)
+screen_rect = screen.get_rect()
+bg = pygame.image.load("data/background-.png").convert()
+bg_rect = background.get_rect()
+
+
+
+
 
